de21006204351cb36f17d6e61f14b99a5391c6fa
alucardxpmusic <705280081@qq.com>
2017-05-12 17:29:17 +0800

    Update post.js

--- js/post.js
+++ js/post.js
@@ -5,7 +5,7 @@ $(function() {
     $('#disqus_container .comment').on('click',function(){
         $(this).html('加载中...');
         var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+        $.getScript('https://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
     });
 
     $('.entry a').each(function(index,element){
